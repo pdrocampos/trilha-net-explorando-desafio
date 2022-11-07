a0e7ee5919c6ad9eb8e4e391ae0ae864b042d9bd
Pedro Henrique <pdrocampos.santos@gmail.com>
2022-11-07 18:35:55 -0300

    Concluindo o desafio

---
+++ Models/Reserva.cs
@@ -0,0 +1,51 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                Console.WriteLine("O número de hospedes ultrapassou a capacidade máxima");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            int numeroH = Hospedes.Count;
+            return numeroH;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * (Suite.ValorDiaria);
+
+            if (DiasReservados >= 10)
+            {
+                return valor = (valor - valor/10);
+            }
+
+            return valor;
+        }
+    }
+}
